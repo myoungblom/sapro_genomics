9004012624e84571f041f07b63678a3d43b920c7
Madison Youngblom <youngblom@wisc.edu>
2023-05-29 12:34:34 -0500

    cfml scripts

--- recombination/run_fastgear.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# runs fastgear on core genome alignment
-
-
-/opt/PepPrograms/fastGEARpackageLinux64bit/run_fastGEAR.sh /opt/PepPrograms/MCRInstallerLinux64bit/v901/ ./2023.01.24_allPrank85_cga.aln ./allPrank85_fastgear.mat /opt/PepPrograms/fastGEARpackageLinux64bit/fG_input_specs.txt
